2374032d015f31389a497df50f8794329b526154
fathurhidayat3 <fathur.hidayat@gmail.com>
2019-07-16 10:54:16 +0700

    Integrate Home to graphql

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import styled from "styled-components";
+import gql from "graphql-tag";
+import { Query } from "react-apollo";
 import Card from "../components/Card";
-import StyledText from "../components/commons/StyledText";
-
-const data = require("../data.json");
-
-const HomeContainer = styled.div`
-  flex: 1;
-  flex-wrap: wrap;
-  display: flex;
-  margin: 25px -8px;
-`;
+import DetailContainer from "../components/DetailContainer";
 
 class Home extends React.Component {
   constructor(props) {
@@ -23,29 +15,51 @@ class Home extends React.Component {
   }
 
   render() {
-    return (
-      <>
-        <StyledText size={"h2"} bold>
-          Pokepixl
-        </StyledText>
+    const GET_ALL_POKEMON = gql`
+      {
+        getAllPokemon {
+          id
+          name
+          abilities
+          types
+          image
+        }
+      }
+    `;
 
-        <HomeContainer>
-          {data.pokemon.map(dataItem => {
-            return (
-              <Link
-                to={{
-                  pathname: `/pokemon/${dataItem.name.toLowerCase()}`,
-                  param1: dataItem
-                }}
-                style={{ flex: 1, display: "flex", color: "unset" }}
-                key={dataItem.id}
-              >
-                <Card data={dataItem} />
-              </Link>
-            );
-          })}
-        </HomeContainer>
-      </>
+    return (
+      <Query query={GET_ALL_POKEMON}>
+        {({ loading, error, data }) => {
+          if (loading) return <div className="empty-state">Loading...</div>;
+          if (error)
+            return <div className="empty-state">Error! ${error.message}</div>;
+
+          return (
+            <>
+              <DetailContainer>
+                {data.getAllPokemon.map(dataItem => {
+                  return (
+                    <Link
+                      to={{
+                        pathname: `/pokemon/${dataItem.name.toLowerCase()}`,
+                        param1: dataItem
+                      }}
+                      style={{
+                        flex: 1,
+                        display: "flex",
+                        color: "unset"
+                      }}
+                      key={dataItem.id}
+                    >
+                      <Card data={dataItem} />
+                    </Link>
+                  );
+                })}
+              </DetailContainer>
+            </>
+          );
+        }}
+      </Query>
     );
   }
 }
