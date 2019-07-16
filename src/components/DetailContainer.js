fd87c74fcb97d45a225c06e6cfb491cbd05e1e95
fathurhidayat3 <fathur.hidayat@gmail.com>
2019-07-16 10:55:13 +0700

    Modify DetailContainer

--- src/components/DetailContainer.js
+++ src/components/DetailContainer.js
@@ -1,6 +1,9 @@
 import styled from "styled-components";
 
 const DetailContainer = styled.div`
+  flex: 1;
+  flex-wrap: wrap;
+  display: flex;
   margin: -16px;
   background: ${props =>
     props.backgroundColor ? props.backgroundColor : "#ffffff"};
