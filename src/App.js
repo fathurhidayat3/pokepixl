c3e5e94e9bf8a5ceb4efcdab284b25c2e15da8e9
fathurhidayat3 <fathur.hidayat@gmail.com>
2019-07-16 13:12:17 +0700

    Refactor App

--- src/App.js
+++ src/App.js
@@ -1,71 +1,38 @@
 import React from "react";
-// Dependencies
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { ApolloClient } from "apollo-client";
 import { HttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { ApolloProvider } from "react-apollo";
-
-import { SidebarContext } from "./contexts/sidebar-context";
-// Pages
 import Home from "./pages/Home";
 import PokemonDetail from "./pages/PokemonDetail";
-// Components
 import ContentWrapper from "./components/ContentWrapper";
 import Navbar from "./components/Navbar";
-import Sidebar from "./components/Sidebar";
 
 const client = new ApolloClient({
   link: new HttpLink({ uri: "http://localhost:8080/graphql" }),
-  cache: new InMemoryCache() // Cache first, or use networkOnly for always use network,
-  // or cacheAndNetwork to check cache first and fetch if there's a change
+  cache: new InMemoryCache()
 });
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      isOpen: false,
-      toggleSidebar: this.toggleSidebar,
-      backgroundColor: "#ffffff",
-      setNavBackgroundColor: this.setNavBackgroundColor
-    };
-  }
-
-  toggleSidebar = () => {
-    this.setState(state => ({
-      isOpen: state.isOpen === true ? false : true
-    }));
-  };
-
-  setNavBackgroundColor = color => {
-    this.setState(state => ({
-      backgroundColor: color
-    }));
-  };
-
   render() {
     return (
       <ApolloProvider client={client}>
         <Router>
-          <SidebarContext.Provider value={this.state}>
-            <div
-              style={{
-                flex: 1,
-                display: "flex",
-                flexDirection: "column",
-                position: "relative"
-              }}
-            >
-              <Navbar />
-              <ContentWrapper>
-                <Route exact path="/" component={Home} />
-                <Route path="/pokemon/:name" component={PokemonDetail} />
-              </ContentWrapper>
-              <Sidebar />
-            </div>
-          </SidebarContext.Provider>
+          <div
+            style={{
+              flex: 1,
+              display: "flex",
+              flexDirection: "column",
+              position: "relative"
+            }}
+          >
+            <Navbar />
+            <ContentWrapper>
+              <Route exact path="/" component={Home} />
+              <Route path="/pokemon/:name" component={PokemonDetail} />
+            </ContentWrapper>
+          </div>
         </Router>
       </ApolloProvider>
     );
